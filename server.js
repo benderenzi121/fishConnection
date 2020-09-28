d0d9f6d7330990f9873767eb5061d0ff67e108b5
benderenzi121 <60165030+benderenzi121@users.noreply.github.com>
2020-09-27 19:47:16 -0700

    added user auth with JWT

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express = require('express');;
+const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
 
 app.get('/', (req , res) => res.send('API is runnig'));
 
-const PORT = process.env.PORT || 5000
+connectDB();
+
+app.use( express.json({ extended: false }));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
